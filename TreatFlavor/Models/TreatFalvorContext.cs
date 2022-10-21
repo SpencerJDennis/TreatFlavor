a58a0db5900b0dfdb4d06dd852b3d6a93399c84a
Spencer Dennis <sjdennis2113@gmail.com>
2022-10-21 16:15:08 -0700

    add boilerplate code and account controller

---
+++ TreatFlavor/Models/TreatFalvorContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TreatFlavor.Models
+{
+  public class TreatFlavorContext : IdentityDbContext<ApplicationUser>
+  {
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<TreatFlavor> TreatFlavor { get; set; }
+
+    public TreatFlavorContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
